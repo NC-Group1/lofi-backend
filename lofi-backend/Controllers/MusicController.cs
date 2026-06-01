6a8a8ec8760799ded3d857d37ab22475e094e5b4
Yee Mon Aung <yeemonaung.isure@icloud.com>
2026-06-01 10:27:26 +0100

    Created Music endpoints for all layers

---
+++ lofi-backend/Controllers/MusicController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using lofi_backend.Data_Models;
+using lofi_backend.Service;
+
+namespace lofi_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MusicController : ControllerBase
+    {
+
+        private readonly IMusicService _service;
+        public MusicController(IMusicService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public IActionResult GetAllMusics()
+        {
+            try
+            {
+                var result = _service.GetAllMusics();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetMusicById(int id)
+        {
+            try
+            {
+                var result = _service.GetMusicById(id);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateMusic([FromBody] Music music)
+        {
+            try
+            {
+                var newMusic = _service.CreateMusic(music);
+                return Ok(newMusic);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return BadRequest();
+            }
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult RemoveMusic(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("Invalid id");
+            }
+
+            Music deletedMusic = _service.RemoveMusic(id);
+
+            if (deletedMusic == null)
+            {
+                return NotFound("Music not found");
+            }
+            return NoContent();
+        }
+    }
+}
---
+++ lofi-backend/Repository/MusicRepository.cs
@@ -0,0 +1,53 @@
+﻿using lofi_backend.Data_Models;
+using lofi_backend.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+
+
+namespace lofi_backend.Repository
+{
+    public interface IMusicRepository
+    {
+
+        public List<Music> GetAllMusics();
+        public Music GetMusicById(int id);
+        public Music CreateMusic(Music music);
+        public Music RemoveMusic(int id);
+    }
+
+    public class MusicRepository : IMusicRepository
+    {
+        private readonly LoFiDbContext _db;
+        public MusicRepository(LoFiDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+        public List<Music> GetAllMusics()
+        {
+            if (_db.Music.ToList().IsNullOrEmpty()) throw new Exception("No musics found");
+            return _db.Music.ToList();
+        }
+        public Music GetMusicById(int id)
+        {
+            return _db.Musics.ToList().First(m => m.Id == id) ?? throw new Exception("Music not found");
+        }
+        public Music CreateMusic(Music music)
+        {
+            if (_db.Musics.Contains(music)) throw new Exception("Music exists");
+            var newMusic = _db.Musics.Add(music).Entity;
+            Console.WriteLine(newMusic.Title + " has been saved");
+            _db.SaveChanges();
+            return newMusic;
+        }
+        public Music RemoveMusic(int id)
+        {
+            var deletedMusic = _db.Musics.First(m => m.Id == id);
+            if (deletedMusic == null)
+                throw new Exception("Music does not exist");
+            _db.Musics.Remove(deletedMusic);
+            _db.SaveChanges();
+            return deletedMusic;
+        }
+    }
+}
