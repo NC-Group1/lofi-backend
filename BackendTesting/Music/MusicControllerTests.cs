0321fed6653e4cf5a4c08341eb2c5a67cb112678
Yee Mon Aung <154083108+YeeMonAung17@users.noreply.github.com>
2026-06-02 09:15:28 +0100

    Music endpoints (#9)
    * Created Music endpoints for all layers
    
    * Added Unit Testing for Music Endpoints

---
+++ BackendTesting/Music/MusicControllerTests.cs
@@ -0,0 +1,144 @@
+﻿using lofi_backend.Controllers;
+using lofi_backend.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.Music
+{
+    internal class MusicControllerTests
+    {
+        private Mock<IMusicService> _mockService;
+        private MusicController _musicController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockService = new Mock<IMusicService>();
+            _musicController = new MusicController(_mockService.Object);
+        }
+
+        [Test]
+        public void GetAllMusics_Returns200Ok()
+        {
+            // Arrange
+            _mockService.Setup(service => service.GetAllMusics()).Returns(new List<lofi_backend.Data_Models.Music>());
+            // Act
+            var result = _musicController.GetAllMusics() as OkObjectResult;
+            // Assert
+            result.ShouldNotBeNull();
+            result.StatusCode.ShouldBe(StatusCodes.Status200OK);
+        }
+
+        [Test]
+        public void GetAllMusics_Returns404NotFound()
+        {
+            // Arrange
+            _mockService.Setup(service => service.GetAllMusics()).Throws(new Exception());
+            // Act
+            var result = _musicController.GetAllMusics() as NotFoundResult;
+            // Assert
+            result.ShouldNotBeNull();
+            result.StatusCode.ShouldBe(StatusCodes.Status404NotFound);
+        }
+
+        [Test]
+        public void GetMusicById_Returns200Ok()
+        {
+            // Arrange
+            _mockService.Setup(service => service.GetMusicById(1)).Returns(new lofi_backend.Data_Models.Music());
+            // Act
+            var result = _musicController.GetMusicById(1) as OkObjectResult;
+            // Assert
+            result.ShouldNotBeNull();
+            result.StatusCode.ShouldBe(StatusCodes.Status200OK);
+        }
+
+        [Test]
+        public void GetMusicById_Returns404NotFound()
+        {
+            // Arrange
+            _mockService.Setup(service => service.GetMusicById(1)).Throws(new Exception());
+            // Act
+            var result = _musicController.GetMusicById(1) as NotFoundResult;
+            // Assert
+            result.ShouldNotBeNull();
+            result.StatusCode.ShouldBe(StatusCodes.Status404NotFound);
+        }
+
+        [Test]
+        public void CreateMusic_ReturnsCreatedAtAction_WhenMusicIsValid()
+        {
+            // Arrange
+            var music = new lofi_backend.Data_Models.Music
+            {
+                Id = 1,
+                Title = "Test Music",
+                Artist = "Test Artist",
+                Channel = "Test Channel",
+                Mood = lofi_backend.Data_Models.Enums.Mood.Romantic,
+                Genre = lofi_backend.Data_Models.Enums.Genre.LoFi,
+                URL = "Test URL"
+            };
+            _mockService.Setup(service => service.CreateMusic(music)).Returns(music);
+            // Act
+            var result = _musicController.CreateMusic(music) as OkObjectResult;
+            // Assert
+            result.ShouldNotBeNull();
+            result.StatusCode.ShouldBe(StatusCodes.Status200OK);
+        }
+
+        [Test]
+        public void CreateMusic_ReturnsBadRequest_WhenMusicIsInvalid()
+        {
+            // Arrange
+            var music = new lofi_backend.Data_Models.Music
+            {
+                Id = 1,
+                Title = "Test Music",
+                Artist = "Test Artist",
+                Channel = "Test Channel",
+                Mood = lofi_backend.Data_Models.Enums.Mood.Romantic,
+                Genre = lofi_backend.Data_Models.Enums.Genre.LoFi,
+                URL = "Test URL"
+            };
+            _mockService.Setup(service => service.CreateMusic(music)).Throws(new Exception());
+            // Act
+            var result = _musicController.CreateMusic(music) as BadRequestResult;
+            // Assert
+            result.ShouldNotBeNull();
+            result.StatusCode.ShouldBe(StatusCodes.Status400BadRequest);
+        }
+
+       
+        [Test]
+        public void CreateMusic_MusicAlreadyExists_Returns400BadRequest()
+        {
+            // Arrange
+            var music = new lofi_backend.Data_Models.Music
+            {
+                Id = 1,
+                Title = "Test Music",
+                Artist = "Test Artist",
+                Channel = "Test Channel",
+                Mood = lofi_backend.Data_Models.Enums.Mood.Romantic,
+                Genre = lofi_backend.Data_Models.Enums.Genre.LoFi,
+                URL = "Test URL"
+            };
+            _mockService.Setup(service => service.CreateMusic(music)).Throws(new Exception());
+            // Act
+            var result = _musicController.CreateMusic(music) as BadRequestResult;
+            // Assert
+            result.ShouldNotBeNull();
+            result.StatusCode.ShouldBe(StatusCodes.Status400BadRequest);
+        }
+    }
+
+
+    }
---
+++ BackendTesting/Music/MusicServiceTests.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using lofi_backend.Repository;
+using lofi_backend.Service;
+using lofi_backend.Data_Models;
+using Shouldly;
+
+
+namespace Testing.Music
+{
+    internal class MusicServiceTests
+    {
+        private Mock<IMusicRepository> _mockRepo;
+        private MusicService _musicService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepo = new Mock<IMusicRepository>();
+            _musicService = new MusicService(_mockRepo.Object);
+        }
+
+        [Test]
+        public void GetAllMusics_ReturnsAllMusics()
+        {
+            // Arrange
+            var musics = new List<lofi_backend.Data_Models.Music>
+            {
+                new lofi_backend.Data_Models.Music
+                {
+                    Id = 1,
+                    Title = "Test Music",
+                    Artist = "Test Artist",
+                    Channel = "Test Channel",
+                    Mood = lofi_backend.Data_Models.Enums.Mood.Romantic,
+                    Genre = lofi_backend.Data_Models.Enums.Genre.LoFi,
+                    URL = "Test URL"
+                },
+
+                new lofi_backend.Data_Models.Music
+                {
+                    Id = 2,
+                    Title = "Test Music 2",
+                    Artist = "Test Artist 2",
+                    Channel = "Test Channel 2",
+                    Mood = lofi_backend.Data_Models.Enums.Mood.Focus,
+                    Genre = lofi_backend.Data_Models.Enums.Genre.HipHop,
+                    URL = "Test URL 2"
+                }
+            };
+            _mockRepo.Setup(repo => repo.GetAllMusics()).Returns(musics);
+            // Act
+            var result = _musicService.GetAllMusics();
+            // Assert
+            result.ShouldBe(musics);
+        }
+
+
+        [Test]
+        public void GetAllMusics_ReturnsEmptyList_WhenNoMusicsExist()
+        {
+            // Arrange
+            var musics = new List<lofi_backend.Data_Models.Music>();
+            _mockRepo.Setup(repo => repo.GetAllMusics()).Returns(musics);
+            // Act
+            var result = _musicService.GetAllMusics();
+            // Assert
+            result.ShouldBeEmpty();
+        }
+
+        [Test]
+        public void GetMusicById_ReturnsMusic()
+        {
+            // Arrange
+            var music = new lofi_backend.Data_Models.Music
+            {
+                Id = 1,
+                Title = "Test Music",
+                Artist = "Test Artist",
+                Channel = "Test Channel",
+                Mood = lofi_backend.Data_Models.Enums.Mood.Romantic,
+                Genre = lofi_backend.Data_Models.Enums.Genre.LoFi,
+                URL = "Test URL"
+            };
+            _mockRepo.Setup(repo => repo.GetMusicById(1)).Returns(music);
+            // Act
+            var result = _musicService.GetMusicById(1);
+            // Assert
+            result.ShouldBe(music);
+        }
+
+        [Test]
+        public void GetMusicById_ReturnsNull_WhenMusicDoesNotExist()
+        {
+            // Arrange
+            _mockRepo.Setup(repo => repo.GetMusicById(1)).Returns((lofi_backend.Data_Models.Music)null);
+            // Act
+            var result = _musicService.GetMusicById(1);
+            // Assert
+            result.ShouldBeNull();
+        }
+
+        [Test]
+        public void CreateMusic_ReturnsCreatedMusic()
+        {
+            // Arrange
+            var musicToCreate = new lofi_backend.Data_Models.Music
+            {
+                Title = "Test Music",
+                Artist = "Test Artist",
+                Channel = "Test Channel",
+                Mood = lofi_backend.Data_Models.Enums.Mood.Romantic,
+                Genre = lofi_backend.Data_Models.Enums.Genre.LoFi,
+                URL = "Test URL"
+            };
+            var createdMusic = new lofi_backend.Data_Models.Music
+            {
+                Id = 1,
+                Title = "Test Music",
+                Artist = "Test Artist",
+                Channel = "Test Channel",
+                Mood = lofi_backend.Data_Models.Enums.Mood.Romantic,
+                Genre = lofi_backend.Data_Models.Enums.Genre.LoFi,
+                URL = "Test URL"
+            };
+            _mockRepo.Setup(repo => repo.CreateMusic(musicToCreate)).Returns(createdMusic);
+            // Act
+            var result = _musicService.CreateMusic(musicToCreate);
+            // Assert
+            result.ShouldBe(createdMusic);
+        }
+
+        [Test]
+        public void RemoveMusic_ReturnsRemovedMusic()
+        {
+            // Arrange
+            var musicId = 1;
+            _mockRepo.Setup(repo => repo.RemoveMusic(musicId));
+            // Act
+            var result = _musicService.RemoveMusic(musicId);
+            // Assert
+            _mockRepo.Verify(repo => repo.RemoveMusic(musicId), Times.Once);
+        }
+    }
+}
