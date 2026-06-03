c3a7794ebf989ee83f60f2ef9588381700abc721
Marcus Paterson <marcus_paterson@outlook.com>
2026-06-03 16:17:01 +0100

    Created Youtube class, Controller, Repository and Service (#12)

---
+++ lofi-backend/Data Models/YoutubeVideo.cs
@@ -0,0 +1,11 @@
+﻿namespace lofi_backend.Data_Models
+{
+    public class YoutubeVideo
+    {
+
+        public string VideoId { get; set; } = "";
+        public string Title { get; set; } = "";
+        public string Channel { get; set; } = "";
+        public string Url { get; set; } = "";
+    }
+}
